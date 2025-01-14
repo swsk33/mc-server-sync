3de44d50c7f10ca53103718661537d65623d4858
守望时空33 <yanhanhui2001a@163.com>
2025-01-14 21:22:45 +0800

    增加部分数据模型

---
+++ pkg/model/result.go
@@ -0,0 +1,28 @@
+package model
+
+// Result 统一的返回响应结果
+type Result[T any] struct {
+	// 操作是否成功
+	Success bool `json:"success"`
+	// 返回的消息
+	Message string `json:"message,omitempty"`
+	// 返回的数据，如果没有数据则为nil
+	Data T `json:"data,omitempty"`
+}
+
+// CreateNullSuccessResult 创建不包含数据的成功结果
+func CreateNullSuccessResult[T any](message string) *Result[T] {
+	var zero T
+	return &Result[T]{true, message, zero}
+}
+
+// CreateSuccessResult 创建成功结果
+func CreateSuccessResult[T any](message string, data T) *Result[T] {
+	return &Result[T]{true, message, data}
+}
+
+// CreateFailedResult 创建失败结果
+func CreateFailedResult[T any](message string) *Result[T] {
+	var zero T
+	return &Result[T]{false, message, zero}
+}
